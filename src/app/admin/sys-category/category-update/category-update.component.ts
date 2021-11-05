127c16f8dc42ff9047e47fe030fc7672b11c2ea9
Nguyen0611 <55937297+Nguyen0611@users.noreply.github.com>
2021-11-05 19:42:35 +0700

    cm

--- src/app/admin/sys-category/category-update/category-update.component.ts
+++ src/app/admin/sys-category/category-update/category-update.component.ts
@@ -69,7 +69,7 @@ export class CategoryUpdateComponent implements OnInit {
         return false;
       }
       if (this.listQue[i].listAnswers === null || this.listQue[i].listAnswers?.length === 0) {
-        this.toastr.danger("Cau hoi" + (i + 1) + " it nhat 1 dap an", this.translate.instant('common.title_notification'));
+        this.toastr.danger("Question" + (i + 1) + "at least one answer", this.translate.instant('common.title_notification'));
         this.inputUser.get("listQue").setValue(null);
         return false;
       } else {
@@ -239,7 +239,7 @@ export class CategoryUpdateComponent implements OnInit {
           this.listQue[obj].listAnswers[i].stt = (i + 1);
         }
       } else {
-        this.toastr.danger("Chi dc toi da 5 dap an", this.translate.instant('common.title_notification'));
+        this.toastr.danger("Maximun is 5 answers", this.translate.instant('common.title_notification'));
       }
 
     }
@@ -249,7 +249,7 @@ export class CategoryUpdateComponent implements OnInit {
   remoteQue(type: any, obj: any, idRemote: any) {
     if (type === 0) {
       if (this.listQue.length <= 1) {
-        this.toastr.danger("1 chu de co it nhat 1 cau hoi", this.translate.instant('common.title_notification'));
+        this.toastr.danger("Category have at least one question", this.translate.instant('common.title_notification'));
         return;
       }
       const index = this.listQue.findIndex(x => x.stt === obj);
@@ -264,7 +264,7 @@ export class CategoryUpdateComponent implements OnInit {
         return;
       }
       if (this.listQue[obj].listAnswers.length <= 1) {
-        this.toastr.danger("1 cau hoi co it nhat 1 cau tl", this.translate.instant('common.title_notification'));
+        this.toastr.danger("1 question have at least a answer", this.translate.instant('common.title_notification'));
         return;
       }
       console.log(this.listQue[obj].listAnswers);
